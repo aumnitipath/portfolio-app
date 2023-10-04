4b428b0474592e776714f1466ed6fe5dbe5655a0
aumnitipath <aumnitipath@outlook.com>
2023-10-05 03:25:15 +0700

    feat:add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold font-signature">Hello world!</h1>
+      <NavBar />
     </>
   );
 }

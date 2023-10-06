e33a3e50dce28415123a0cefa3a302a7a310921a
aumnitipath <aumnitipath@outlook.com>
2023-10-06 17:32:11 +0700

    feat:add logic navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [navbar, serNavbar] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -24,6 +26,7 @@ const NavBar = () => {
       link: "contact",
     },
   ];
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4   text-white bg-black fixed ">
       <div>
@@ -43,9 +46,27 @@ const NavBar = () => {
         })}
       </ul>
 
-      <div>
-        <FaTimes />
+      <div
+        onClick={() => serNavbar(!navbar)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {navbar ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {navbar && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map((item) => {
+            return (
+              <li
+                key={item.id}
+                className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              >
+                {item.link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };

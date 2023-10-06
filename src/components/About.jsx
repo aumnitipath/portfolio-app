e1ca1f4d55de1d3ea7f92dcae309865578e242ba
aumnitipath <aumnitipath@outlook.com>
2023-10-07 06:26:23 +0700

    fix:responsive mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,11 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full bg-gradient-to-b from-gray-800 to-black text-white sm:h-screen pt-14"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+        <div className="pb-2">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500  ">
             About
           </p>
         </div>
